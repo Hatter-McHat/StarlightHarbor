662e0bc635b2162701decd672fd34555a50ebb14
Owner <Owner@DESKTOP>
2024-08-12 10:18:45 -0700

    Updated ModEntry.cs

--- StarlightHarbor/Mod/ModEntry.cs
+++ StarlightHarbor/Mod/ModEntry.cs
@@ -12,8 +12,17 @@ using Microsoft.Extensions.Logging;
 
 namespace StarlightHarbor.Mod
 {
-    public sealed partial class ModEntry 
+    //public sealed partial class ModEntry 
+    //copied relevant bits from Soggins
+    public sealed class ModEntry : CobaltCoreModding.Definitions.ModManifests.IModManifest
     {
+        public IEnumerable<DependencyEntry> Dependencies => [
+        new DependencyEntry<CobaltCoreModding.Definitions.ModManifests.IModManifest>("Shockah.Kokoro", ignoreIfMissing: false),
+         ];
+        public DirectoryInfo? GameRootFolder { get; set; }
+        public DirectoryInfo? ModRootFolder { get; set; }
+        public ILogger? Logger { get; set; }
+        public string Name { get; init; } = typeof(ModEntry).Namespace!;
         internal static ModEntry Instance { get; private set; } = null!;
         internal IKokoroApi KokoroApi { get; private set; } = null!;
         public void BootMod(IModLoaderContact contact)
